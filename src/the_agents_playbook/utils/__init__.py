d276ea6763ede31119a1fe0ca22375fccc42a72c
Quan Hua <quanhua92@gmail.com>
2026-04-30 01:31:14 +0700

    feat: add cosine_similarity and normalize vector utilities using numpy

--- src/the_agents_playbook/utils/__init__.py
+++ src/the_agents_playbook/utils/__init__.py
@@ -1,3 +1,4 @@
 from the_agents_playbook.utils.schema import flatten_json_schema
+from the_agents_playbook.utils.vectors import cosine_similarity, normalize
 
-__all__ = ["flatten_json_schema"]
+__all__ = ["flatten_json_schema", "cosine_similarity", "normalize"]
